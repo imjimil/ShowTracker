4e1d1cd0b0f8ad3a8349b1c6df457de74bcb4c60
abc <prajapatijimil@yahoo.com>
2021-05-01 11:38:32 +0530

    adding to gh pages

--- backend-server/config/keys.js
+++ backend-server/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURL: 'mongodb://localhost:27017/userdatabase',
+    mongoURL: 'mongodb+srv://jimil:jimil1308@cluster0.jlw3n.mongodb.net/userdatabase?retryWrites=true&w=majority',
     secret: '51811-42966-52626-65419'
 }
