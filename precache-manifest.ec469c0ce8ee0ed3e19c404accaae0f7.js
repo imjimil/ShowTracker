0c8ccb54bab471d0e8cb9bd679239cde825a79fc
abc <prajapatijimil@yahoo.com>
2021-05-01 11:40:26 +0530

    Updates

---
+++ precache-manifest.ec469c0ce8ee0ed3e19c404accaae0f7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daaf10a3c637cd4489c9f5a7e89c674d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac0539eea8df8cdeef02",
+    "url": "/static/css/2.1c2bea9b.chunk.css"
+  },
+  {
+    "revision": "e063de15bdf91b46a5a7",
+    "url": "/static/css/main.0037a22d.chunk.css"
+  },
+  {
+    "revision": "ac0539eea8df8cdeef02",
+    "url": "/static/js/2.9d3602f9.chunk.js"
+  },
+  {
+    "revision": "1b9cc396521eecb2045c787139670fba",
+    "url": "/static/js/2.9d3602f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e063de15bdf91b46a5a7",
+    "url": "/static/js/main.966a4c08.chunk.js"
+  },
+  {
+    "revision": "a8167dd4a542cc2974ca",
+    "url": "/static/js/runtime-main.84b91ca9.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
